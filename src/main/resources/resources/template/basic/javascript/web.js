c17681c4323c4d285ca4baa799d266080cb33377
Alexander Verbruggen <alex@nabu.be>
2017-01-10 14:39:05 +0100

    Updated

--- src/main/resources/resources/template/basic/javascript/web.js
+++ src/main/resources/resources/template/basic/javascript/web.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function () {
+	// make sure we can target the browser using css
+	document.documentElement.setAttribute("data-useragent", navigator.userAgent);
 	// initialize vue
 	application.initialize.vue();
 	// route to initial state
